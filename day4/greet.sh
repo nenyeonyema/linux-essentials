1edff0e1e2231c7d5d169dbdcdcf8ff995e1b5ca
nenyeonyema <chinenyeonyema1@gmail.com>
2025-04-21 14:46:25 +0000

    day5: packages and services

--- day4/greet.sh
+++ day4/greet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Hello, $NAME! Welcome to Day 4 of my DevOps learning series; Linux Essentials"
+echo "Hello, $MY_NAME! Welcome to Day 4 of my DevOps learning series; Linux Essentials"
 echo "You're currently learning at the $DEVOPS_LEVEL level!"
